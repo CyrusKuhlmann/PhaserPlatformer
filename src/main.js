7c2094e0bca517f10603eef3a69e8aaffada86f0
Greg Kuhlmann <greg.kuhlmann@gmail.com>
2025-05-18 00:12:52 -0500

    formatting

--- src/main.js
+++ src/main.js
@@ -1,21 +1,19 @@
-import * as Phaser from 'phaser';
-import { Start } from './scenes/Start.js';
+import * as Phaser from "phaser";
+import { Start } from "./scenes/Start.js";
 
 const config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    parent: 'game-container',
-    scene: [
-        Start
-    ],
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 300 },
-            debug: false
-        }
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  parent: "game-container",
+  scene: [Start],
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 300 },
+      debug: false,
     },
-}
+  },
+};
 
 new Phaser.Game(config);
