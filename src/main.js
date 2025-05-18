25c9d4e811558f5bb707ecb8a4ffb0ea4e2cedd5
Greg Kuhlmann <greg.kuhlmann@gmail.com>
2025-05-17 23:36:45 -0500

    platformer game

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import { Start } from './scenes/Start.js';
+
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    parent: 'game-container',
+    scene: [
+        Start
+    ],
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
+}
+
+new Phaser.Game(config);
