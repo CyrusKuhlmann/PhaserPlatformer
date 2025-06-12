8470a49a0a07e11f2ca461a0020e0747a9f5725e
CyrusKuhlmann <cyrus.kuhlmann@gmail.com>
2025-06-11 20:39:08 -0500

    added 'ray casting' to the goblin

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -9,7 +9,7 @@ import treeAssets from "/assets\/Legacy-Fantasy - High Forest 2.3\/Assets\/Tree-
 import forest1 from "/assets/maps/Forest1.json";
 import collect from "/assets/sounds/collect.mp3";
 import bounce from "/assets/sounds/bounce.mp3";
-
+import goblinIdle from "/assets/Monsters_Creatures_Fantasy/Goblin/Idle.png"
 
 export class Game extends Phaser.Scene {
   constructor() {
@@ -24,6 +24,10 @@ export class Game extends Phaser.Scene {
     this.load.spritesheet("viking", viking, {
       frameWidth: 115,
       frameHeight: 84,
+    });
+    this.load.spritesheet("goblinIdle", goblinIdle, {
+      frameWidth: 150,
+      frameHeight: 150,
     });
     this.load.image("background", background);
     this.load.image("tiles", tiles);
@@ -47,7 +51,6 @@ export class Game extends Phaser.Scene {
     this.dashing = false;
 
 
-
     // Create layers
     level.createLayer("Light Mountains (background)", tilesets, 0, 0).setScrollFactor(0.4, 0);
     level.createLayer("Light Trees (background)", tilesets, 0, 0).setScrollFactor(0.4, 0);
@@ -63,32 +66,122 @@ export class Game extends Phaser.Scene {
     level.createLayer("Rocks (foreground)", tilesets, 0, 0);
     level.createLayer("Bushes (foreground)", tilesets, 0, 0);
     level.createLayer("Cave Background (background)", tilesets, 0, 0);
-    const ground = level
+    this.ground = level
       .createLayer("Ground (foreground)", tilesets, 0, 0)
       .setCollisionBetween(1, 10000); // Choose which tile IDs collide
     level.createLayer("Grass (foreground)", tilesets, 0, 0);
     level.createLayer("Overhangs (foreground)", tilesets, 0, 0);
 
     this.hero = this.physics.add
-      .sprite(32, height - 100, "viking")
+      .sprite(400, height - 100, "viking")
       .setOrigin(0.5, 1)
       .setBounce(0)
       .setScale(1)
       .setCollideWorldBounds(true);
+    this.hero.body.setSize(33, 46).setOffset(41, 24);
+    this.hero.hit = false;
+
+    this.goblin = this.physics.add
+      .sprite(550, height - 100, "goblinIdle")
+      .setOrigin(0.5, 1)
+      .setBounce(0)
+      .setScale(1)
+      .setCollideWorldBounds(true);
+    this.goblin.body.setSize(40, 35).setOffset(55, 65);
+    this.goblin.setVelocityX(100)
+    this.goblin.hit = false;
+    this.goblin.hits = 0;
 
     this.weaponHitbox = this.physics.add
-      .sprite(this.hero.x + 17.5, this.hero.y - 40, null)
-      .setSize(20, 30)
+      .sprite(this.hero.x + 22.5, this.hero.y - 40, null)
+      .setSize(25, 30)
       .setOrigin(.5, .5)
       .setVisible(false)
       .setImmovable(true);
-
     this.weaponHitbox.body.allowGravity = false;
 
+    // creat overlap between the goblin and the player
+    this.physics.add.overlap(
+      this.hero,
+      this.goblin,
+      (hero, goblin) => {
+        if (!hero.hit) {
+          console.log("Hero hit goblin");
+          hero.hit = true; // Prevent multiple hits
+          this.sound.play("bounce");
+          const oldVelocity = goblin.body.velocity.x;
+          if (goblin.flipX) { // goblin is facing left
+            hero.setVelocityX(-200);
+            this.tweens.add({
+              targets: hero.body.velocity,
+              x: oldVelocity,
+              duration: 500,
+              ease: "Power1",
+            });
+          }
+          else if (!goblin.flipX) { // goblin is facing right
+            hero.setVelocityX(200);
+            this.tweens.add({
+              targets: hero.body.velocity,
+              x: oldVelocity,
+              duration: 500,
+              ease: "Power1",
+            });
+          }
+          hero.setTint(0xff0000);
+          this.time.delayedCall(500, () => {
+            hero.hit = false; // Reset hit state after delay
+            hero.clearTint();
+          });
+        }
+      },
+      null,
+      this
+    );
 
-
-    // Set the hero's bounding box
-    this.hero.body.setSize(33, 46).setOffset(41, 24);
+    // create overlap between the weapon hitbox and the goblin
+    this.physics.add.overlap(
+      this.weaponHitbox,
+      this.goblin,
+      (hitbox, goblin) => {
+        if (this.attacking && hitbox.body.enable && !goblin.hit) {
+          this.goblin.hits += 1;
+          goblin.hit = true; // Prevent multiple hits
+          this.sound.play("collect");
+          if (this.goblin.hits >= 3) {
+            this.goblin.destroy();
+            this.goblin.hits = 0;
+            return;
+          }
+          const oldVelocity = goblin.body.velocity.x;
+          if (this.hero.flipX) { // hero is facing left
+            goblin.setVelocityX(-200);
+            this.tweens.add({
+              targets: goblin.body.velocity,
+              x: oldVelocity,
+              duration: 500,
+              ease: "Power1",
+            });
+          }
+          else if (!this.hero.flipX) { // hero is facing right
+            goblin.setVelocityX(200);
+            this.tweens.add({
+              targets: goblin.body.velocity,
+              x: oldVelocity,
+              duration: 500,
+              ease: "Power1",
+            });
+          }
+          goblin.setTint(0xff0000);
+          this.time.delayedCall(500, () => {
+            goblin.hit = false; // Reset hit state after delay
+            goblin.clearTint();
+          });
+        }
+      },
+      null,
+      this
+    );
 
     this.anims.create({
       key: "idle",
@@ -150,8 +243,15 @@ export class Game extends Phaser.Scene {
       frameRate: 15,
       repeat: 0,
     });
+    this.anims.create({
+      key: "goblinIdle",
+      frames: this.anims.generateFrameNumbers("goblinIdle", { start: 0, end: 3 }),
+      frameRate: 7,
+      repeat: 0,
+    });
 
-    this.physics.add.collider(this.hero, ground);
+    this.physics.add.collider(this.hero, this.ground);
+    this.physics.add.collider(this.goblin, this.ground);
 
     // Set up camera
     this.cameras.main.setBounds(
@@ -173,13 +273,6 @@ export class Game extends Phaser.Scene {
     this.cursors = this.input.keyboard.createCursorKeys();
     this.shift = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SHIFT)
     this.Z = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Z);
-    this.X = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.X);
-    this.C = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.C);
-
-    this.hero.on("animationcomplete", () => {
-      // Reset attacking state when any animation completes
-      this.attacking = false;
-    });
 
 
     // on key down of the x key, play animation
@@ -192,19 +285,10 @@ export class Game extends Phaser.Scene {
       else {
         this.hero.anims.play(`attack${attackNum}`, true);
       }
-      if (this.hero.flipX) {
-        this.hero.setVelocityX(-50)
-        this.weaponHitbox.setVelocityX(-50);
-      }
-      else {
-        this.hero.setVelocityX(50)
-        this.weaponHitbox.setVelocityX(50);
-      }
       // Enable and position the weapon hitbox
       this.weaponHitbox.setVisible(false);
       this.weaponHitbox.body.enable = true;
-      this.weaponHitbox.x = this.hero.x + (this.hero.flipX ? -17.5 : 17.5);
-      this.weaponHitbox.y = this.hero.y - 40;
+
     });
 
     // ...when attack animation completes, hide and disable the hitbox...
@@ -225,15 +309,39 @@ export class Game extends Phaser.Scene {
       this.dashing = true;
       this.hero.anims.play("dash", true);
       if (this.hero.flipX) {
-        this.hero.setVelocityX(-150);
+        this.hero.setVelocityX(-175);
       }
       else {
-        this.hero.setVelocityX(150);
+        this.hero.setVelocityX(175);
       }
     });
   }
 
   update() {
+    const edgeOffset = this.goblin.flipX ? -this.goblin.body.width / 2 - 1 : this.goblin.body.width / 2 + 1;
+    const checkX = this.goblin.x + edgeOffset;
+    const checkY = this.goblin.y + 2;
+
+    const edgeTile = this.ground.getTileAtWorldXY(checkX, checkY);
+
+    if (!edgeTile || !edgeTile.collides) {
+      this.goblin.flipX = !this.goblin.flipX;
+      this.goblin.setVelocityX(this.goblin.flipX ? -75 : 75);
+    }
+
+    this.goblin.anims.play("goblinIdle", true);
+    if (this.goblin.body.blocked.right) {
+      this.goblin.flipX = true;
+      this.goblin.setVelocityX(-75)
+    }
+    else if (this.goblin.body.blocked.left) {
+      this.goblin.flipX = false;
+      this.goblin.setVelocityX(75)
+    }
+
+    this.weaponHitbox.x = this.hero.x + (this.hero.flipX ? -17.5 : 17.5);
+    this.weaponHitbox.y = this.hero.y - 40;
+
     if (this.cursors.left.isDown && !this.dashing) {
       if (this.shift.isDown) { // Check if Shift is pressed
         this.hero.setVelocityX(-200);
@@ -252,7 +360,7 @@ export class Game extends Phaser.Scene {
       if (this.shift.isDown) { // Check if Shift is pressed
         this.hero.setVelocityX(200);
         this.hero.flipX = false;
-        if (this.hero.body.blocked.down && !this.attacking); {
+        if (this.hero.body.blocked.down && !this.attacking) {
           this.hero.anims.play("run", true);
         }
       } else {
